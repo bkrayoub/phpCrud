94212940ba93b97adeeb18fc0b3a1cf4a138099b
bkrayoub <boukkour.mohamed.solicode@gmail.com>
2022-10-17 22:01:14 +0100

    done

--- presentation/add_prom.php
+++ presentation/add_prom.php
@@ -5,12 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add promotion</title>
 </head>
 
 <body>
     <form action="../business/insert_prom.php" method="POST">
-        <label for="">Nom: <input type="text" name="nom"></label>
+        <label for="">Name: <input type="text" name="nom"></label>
         <input type="submit" name="add_prom" id="">
         <p id="message"></p>
     </form>
